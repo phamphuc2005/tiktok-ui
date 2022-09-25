05fb132c410e8e1c97a248706e7276c1920ac734
phamphuc2005 <minhpham2001bk@gmail.com>
2022-09-25 10:53:56 +0700

    toi uu code

--- src/layouts/components/Header/index.js
+++ src/layouts/components/Header/index.js
@@ -21,7 +21,7 @@ import { InboxIcon, MessageIcon, UploadIcon } from '~/component/Icons';
 import Image from '~/component/Image';
 import Search from '../Search';
 import { Link } from 'react-router-dom';
-import routesConfig from '~/config/routes';
+import config from '~/config';
 
 const cx = classNames.bind(styles);
 
@@ -89,7 +89,7 @@ function Header() {
   return (
     <header className={cx('wrapper')}>
       <div className={cx('inner')}>
-        <Link to={routesConfig.home} className={cx('logo-link')}>
+        <Link to={config.routes.home} className={cx('logo-link')}>
           <img src={image.logo} alt="tiktok" />
         </Link>
 
